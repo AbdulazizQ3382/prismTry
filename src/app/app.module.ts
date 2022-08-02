29fa72da133b8c9a045afa45796c80a70cd9d0d1
aqannam <aqannam@nic.gov.sa>
2022-08-02 15:53:19 +0300

    prism with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HighlightService} from "../services/highlight.service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    HighlightService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
